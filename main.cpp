2603646ce5361d8b4de1e57af9a645032570639f
Aviral <aviral1101@gmail.com>
2020-08-17 20:40:28 +0530

    Class added

--- main.cpp
+++ main.cpp
@@ -1,5 +1,21 @@
 #include<iostream>
 using namespace std;
+class Account {
+private:
+	long long accountNumber;
+	string firstName;
+	string lastName;
+	float balance;
+	static long NextAccountNumber;
+public:
+	Account() {}
+	Account(string fname, string lname, float balance);
+	long getAccNo() {return accountNumber;}
+	string getFirstName() {return firstName;}
+	string getLastName() {return lastName;}
+	float getBalance() {return balance;}
+
+}
 int main() {
 	return 0;
 }
