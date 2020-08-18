fc6759bd7fc77542e95aa73b84c79632f67e30b8
Aviral <aviral1101@gmail.com>
2020-08-18 16:49:20 +0530

    check

--- main.cpp
+++ main.cpp
@@ -1,4 +1,7 @@
 #include<iostream>
+#include<cstring>
+#include<map>
+#include<fstream>
 using namespace std;
 class Account {
 private:
@@ -15,7 +18,38 @@ public:
 	string getLastName() {return lastName;}
 	float getBalance() {return balance;}
 
+	void Deposit(float amount);
+	void Withdraw(float amount);
+	static void setLastAccountNumber(long accountNumber);
+	static long getLastAccountNumber();
+	friend ofstream & operator<<(ofstream &ofs, Account &acc);
+	friend ifstream & operator>>(ifstream &ifs, Account, &acc);
+	friend ofstream & operator<<(ofstream, &os, Account, &acc);
+
+};
+
+Account::Account(string fname, string lname, foat balance) {
+	NextAccountNumber++;// why are we doing this let me see afterwards
+	accountNumber = NextAccountNumber;
+	firstName = fname;
+	lastName = lname;
+	this->balance = balance;
 }
+void Account::Deposit(float amount) {
+	balance += amount;
+}
+void Account::Withdraw(float amount) {
+	balance -= amount;
+}
+
+void Account::setLastAccountNumber(long accountNumber) {
+	NextAccountNumber = accountNumber;
+}
+void Account::getLastAccountNumber() {
+	return NextAccountNumber;
+}
+
+
 int main() {
 	return 0;
 }
